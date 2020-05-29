4ed271ab1583e60b0d34e39c376fb4fa6e408006
Shakore McGill <mcgills2093@gmail.com>
2020-05-29 21:37:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,7 @@
 def run_guessing_game
   user_input = gets.chomp
   computer_number = rand(6) + 1
+  computer_number = "#{computer_number}"
   if user_input == 'exit'
     puts "Goodbye!"
   elsif user_input == computer_number
